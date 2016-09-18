c8add6ad433491929f716eed89d9bdbe4195c6b7
Allen Hendricks <allencoded@gmail.com>
2016-09-18 04:41:59 -0400

    Bootstrap 3
    Added a bootstrap 3 skeleton

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, Ng2BootstrapModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- systemjs.config.js
+++ systemjs.config.js
@@ -4,9 +4,10 @@
  */
 (function (global) {
   System.config({
+    'defaultJSExtensions':true,
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': 'node_modules/',
     },
     // map tells the System loader where to look for things
     map: {
@@ -21,6 +22,8 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      'ng2-bootstrap': 'npm:ng2-bootstrap',
+      'moment': 'npm:moment/moment.js',
       // other libraries
       'rxjs':                       'npm:rxjs',
       'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
